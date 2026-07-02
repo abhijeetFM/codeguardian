ab86e83fab3fe557aecd07db170a7b183c256716
prashant30200501 <jangidprashant28@gmail.com>
2026-07-02 12:12:22 +0530

    add import extractction

---
+++ src/extractor/import_extractor.py
@@ -0,0 +1,23 @@
+from tree_sitter import Node
+from src.tree.Walker import walk
+
+
+class importExtractor:
+    def __init__(self):
+        self.imports =[]
+
+    def visit(self, node):
+
+      if node.type != "import_statement":
+          return
+
+      for child in node.children:
+
+          if child.type == "string":
+
+              self.imports.append(
+                child.text.decode("utf8").strip('"')
+            )
+
+  
+
